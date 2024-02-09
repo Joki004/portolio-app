35df532a3598a132faea8457f378f00c984e649f
Joram Mumb <jorammumb15.jm@gmail.com>
2024-02-09 19:29:25 +0100

    rendering project component

--- src/pages/projects/projects.js
+++ src/pages/projects/projects.js
@@ -1,9 +1,223 @@
+import React, { useState, useEffect } from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { useElements } from "../../utils/functions/context";
 
-const Projects = () => {
+const Technologies = ({ Technologies }) => {
+  return (
+    <div style={{ display: "flex", gap: "10px" }}>
+      {Technologies.map((technology, idx) => (
+        <p key={idx}>{technology}</p>
+      ))}
+    </div>
+  );
+};
+
+const ProjectDetails = ({ project }) => {
   return (
     <div>
-      <h1>Projects</h1>
-      <p>Here are some of my projects</p>
+      <h2>{project.name}</h2>
+      <p>{project.description}</p>
+      <div>
+        <Technologies Technologies={project.technologies} />
+      </div>
+      <div>
+        {project.weblink !== " " ? <a>See </a> : null}
+        <a>Github Link</a>
+      </div>
+    </div>
+  );
+};
+
+const ProjectImages = (images) => {
+  const { mainColor10Lighter } = useElements();
+  const projectImagesStyles = {
+    box: {
+      display: "flex",
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "center",
+
+      gap: "10px",
+      height: "100%",
+      width: "65%",
+      padding: "10px",
+    },
+    carrousel: {
+      width: "100%",
+      height: "100%",
+
+      display: "flex",
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "center",
+      borderRadius: "10px",
+    },
+    imageBox: {
+      width: "100%",
+      height: "100%",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      border: "3px solid",
+      borderColor: mainColor10Lighter,
+      borderRadius: "10px",
+    },
+  };
+  const [autoplay, setAutoplay] = useState(true);
+  const toggleAutoplay = () => {
+    setAutoplay(!autoplay);
+  };
+  const { images: imageArray } = images;
+
+  return (
+    <div style={{ ...projectImagesStyles.box }}>
+      <Carousel
+        showThumbs={false}
+        showStatus={false}
+        showArrows={true}
+        showIndicators={false}
+        autoPlay={autoplay}
+        interval={5000}
+        infiniteLoop
+        stopOnHover
+        style={{ border: "1px solid red" }}
+      >
+        {imageArray.map((imageUrl, index) => (
+          <div key={index} style={{ ...projectImagesStyles.carrousel }}>
+            <div style={{ ...projectImagesStyles.imageBox }}>
+              <img
+                src={imageUrl}
+                alt={`Project  ${index}`}
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  margin: "0 auto",
+                  objectFit: "contain",
+                  borderRadius: "10px",
+                }}
+              />
+            </div>
+          </div>
+        ))}
+      </Carousel>
+
+      <button onClick={toggleAutoplay}>
+        {autoplay ? "Pause" : "Play"} Autoplay
+      </button>
+    </div>
+  );
+};
+
+const projectStyles = {
+  image: {
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  smallScreenBox: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+
+    margin: "10px",
+  },
+  bigScreen: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+};
+
+const Projects = ({ projectsData }) => {
+  const [autoplay, setAutoplay] = useState(true);
+  const { windowWidth } = useElements();
+  const [isScreenSmall, setIsScreenSmall] = useState(false);
+
+  useEffect(() => {
+    if (windowWidth < 1000) {
+      setIsScreenSmall(true);
+    } else {
+      setIsScreenSmall(false);
+    }
+  }, [windowWidth]);
+  const toggleAutoplay = () => {
+    setAutoplay(!autoplay);
+  };
+
+  // Helper function to chunk the projects into groups of 3
+  const chunkProjects = (projects, size) => {
+    const chunkedProjects = [];
+    for (let i = 0; i < projects.length; i += size) {
+      chunkedProjects.push(projects.slice(i, i + size));
+    }
+    return chunkedProjects;
+  };
+
+  const chunkedProjects = chunkProjects(projectsData, 3); // Split projects into groups of 3
+  const RenderBigScreen = ({ index, project }) => {
+    if (index % 2 === 0) {
+      return (
+        <div style={{ ...projectStyles.bigScreen }}>
+          <ProjectDetails project={project} />
+          <div style={{ ...projectStyles.image }}>
+            <ProjectImages images={project.images[0]} />
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div style={{ ...projectStyles.smallScreenBox }}>
+          <div style={{ ...projectStyles.image }}>
+            <ProjectImages images={project.images[0]} />
+          </div>
+          <ProjectDetails project={project} />
+        </div>
+      );
+    }
+  };
+  return (
+    <div>
+      <Carousel
+        showThumbs={false}
+        showStatus={false}
+        showArrows={true}
+        showIndicators={false}
+        autoPlay={autoplay}
+        interval={5000} // Change interval duration as needed
+        infiniteLoop
+        stopOnHover
+      >
+        {chunkedProjects.map((group, index) => (
+          <div key={index}>
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              {group.map((project, idx) => (
+                <div key={idx} style={{ flex: 1, margin: "0 10px" }}>
+                  {isScreenSmall ? (
+                    <div style={{ ...projectStyles.smallScreenBox }}>
+                      <ProjectDetails project={project} />
+                      <div style={{ ...projectStyles.image }}>
+                        <ProjectImages images={project.images[0]} />
+                      </div>
+                    </div>
+                  ) : (
+                    <RenderBigScreen index={idx} project={project} />
+                  )}
+
+                  {/* Add more project details as needed */}
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </Carousel>
+      <button onClick={toggleAutoplay}>
+        {autoplay ? "Pause" : "Play"} Autoplay
+      </button>
     </div>
   );
 };
--- src/utils/datas.js
+++ src/utils/datas.js
@@ -1,5 +1,6 @@
 import Home from "../pages/home/home";
 import AboutMe from "../pages/aboutMe/aboutMe";
+import Projects from "../pages/projects/projects";
 let text =
   "Section 1Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sollicitudin condimentum odio, et interdum est fringilla eget. Praesent risus dui, dapibus vel elementum sed, volutpat id sem. In fermentum, arcLorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sollicitudin condimentum odio, et interdum est fringilla eget. Praesent risus dui, dapibus vel elementum sed, volutpat id sem. In fermentum, arcu vitae pharetra malesuada, massa urna interdum erat, vel interdum felis mi quis lacus. Nunc nec erat faucibus, feugiat mauris facilisis, sagittis dolor. Donec pellentesque quam et ipsum faucibus tempus. Praesent lacinia quam leo, et vehicula dui maximus sed. In augue eros, elementum nec accumsan nec, pretium ac turpis. Donec tristique nec orci id rhoncus. Proin eu sem nec nulla dictum fermentum eu vitae nunc. Proin in elit metus. Aenean vel nulla augue. Vivamus ante odio, dictum et dignissim eu, fringilla quis tellus. Quisque lectus enim, vulputate vitae orci quis, pellentesque tempor justo. Cras at fermentum magna. Nulla bibendum turpis vel rutrum aliquam. Ut lacus est, dignissim ut ante eget, interdum auctor metus. Vestibulum aliquam ipsum rutrum tellus imperdiet, fermentum pellentesque ligula mattis. Nullam blandit augue sit amet dictum sagittis. Curabitur pretium gravida lacus. Sed ullamcorper mi sed condimentum viverra. Duis sed libero metus. In lobortis metus egestas, euismod mi sit amet, volutpat enim. Aenean bibendum justo vitae nibh facilisis rutrum. Praesent fringilla felis sit amet rhoncus finibus. Vivamus et lacus quis lacus ullamcorper tristique eu a sem. Maecenas eu condimentum neque. Fusce hendrerit nunc sit amet magna iaculis, nec tincidunt massa rutrum. Fusce mi elit, molestie ac dapibus quis, ultricies eget est. Aliquam tincidunt augue id sollicitudin auctor. Nullam interdum eget risus in pharetra. In interdum nisi erat, sed posuere mi viverra in. Nullam sed tortor in arcu aliquet feugiat eu sed augue. Ut tincidunt at elit in dapibus. Morbi vitae posuere massa, quis dignissim odio. In rutrum libero odio, vel tristique mauris ultrices vel. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.";
 const imageURL = require("../../src/assets/images/PictureOfMe.jpg");
@@ -23,26 +24,105 @@ const aboutMeText = {
 };
 
 const languageData = [
-  { language: "French", proficiency: 100,level:"Native" },
-  { language: "Shwahili", proficiency: 100 , level:"Native" },
-  { language: "English", proficiency: 95, level:"Fluent"},
-  { language: "Polish", proficiency: 85, level:"Fluent"},
-  { language: "Spanish", proficiency: 50, level:"Intermediate"},
- 
-  
+  { language: "French", proficiency: 100, level: "Native" },
+  { language: "Shwahili", proficiency: 100, level: "Native" },
+  { language: "English", proficiency: 95, level: "Fluent" },
+  { language: "Polish", proficiency: 85, level: "Fluent" },
+  { language: "Spanish", proficiency: 50, level: "Intermediate" },
 ];
 
-const zfDescription=`"I've been an essential contributor to the 'Innovation' project at Lodz University of Technology, where we partnered with industry leaders to tackle real-world challenges. My focus revolved around developing a platform leveraging MySQL, Ansible, HTML, and PHP to automate software installations and configurations on virtual machines within the ZF network. From enabling seamless machine selection to facilitating the installation process, my role was instrumental in optimizing our workflow and enhancing efficiency."`
-const freeLance = `During 2018-2019, I freelanced as a small business manager, overseeing operations for a taxi service. My responsibilities included tracking entries and expenses meticulously using Excel sheets. Additionally, I collaborated with companies specializing in data sales for online platforms, ensuring accurate record-keeping and efficient management of our business operations.`
+const zfDescription = `I've been an essential contributor to the 'Innovation' project at Lodz University of Technology, where we partnered with industry leaders to tackle real-world challenges. My focus revolved around developing a platform leveraging MySQL, Ansible, HTML, and PHP to automate software installations and configurations on virtual machines within the ZF network. From enabling seamless machine selection to facilitating the installation process, my role was instrumental in optimizing our workflow and enhancing efficiency."`;
+const freeLance = `During 2018-2019, I freelanced as a small business manager, overseeing operations for a taxi service. My responsibilities included tracking entries and expenses meticulously using Excel sheets. Additionally, I collaborated with companies specializing in data sales for online platforms, ensuring accurate record-keeping and efficient management of our business operations.`;
 
-const SSSDescription = `I've been part of a groundbreaking project named 'Innovation.' Here, at Lodz University of Technology, we collaborated with leading companies, tackling real-world tasks throughout the semester. I took charge of developing the frontend interface using React Native, enhancing user experience and pushing boundaries!" 🚀`
-const universityDescription = `I am currently pursuing a Bachelor's degree in Computer Science at Lodz University of Technology, where I've been honing my skills in software development, data structures, and algorithms.`
- const timelineData=[
-    {date:'11-2023 - 02-2024', organization: 'Soft Smart Solution', position: 'Internship',description:SSSDescription},
-    {date:'04-2023 - 09-2023', organization: 'ZF friedrichshafen', position: 'Internship',description:zfDescription},
-    {date:"2021 - present", organization:'Politechnika Łódzka', position:'Bachelor Degree in Computer Science', description: universityDescription},
-    {date:"2018 - 2019", organization:'Free lance', position:'management of a small business',description:freeLance},   
+const SSSDescription = `I've been part of a groundbreaking project named 'Innovation.' Here, at Lodz University of Technology, we collaborated with leading companies, tackling real-world tasks throughout the semester. I took charge of developing the frontend interface using React Native, enhancing user experience and pushing boundaries!" 🚀`;
+const universityDescription = `I am currently pursuing a Bachelor's degree in Computer Science at Lodz University of Technology, where I've been honing my skills in software development, data structures, and algorithms.`;
+const timelineData = [
+  {
+    date: "11-2023 - 02-2024",
+    organization: "Soft Smart Solution",
+    position: "Internship",
+    description: SSSDescription,
+  },
+  {
+    date: "04-2023 - 09-2023",
+    organization: "ZF friedrichshafen",
+    position: "Internship",
+    description: zfDescription,
+  },
+  {
+    date: "2021 - present",
+    organization: "Politechnika Łódzka",
+    position: "Bachelor Degree in Computer Science",
+    description: universityDescription,
+  },
+  {
+    date: "2018 - 2019",
+    organization: "Free lance",
+    position: "management of a small business",
+    description: freeLance,
+  },
+];
 
+const projectImage =
+  "https://images.unsplash.com/photo-1499209974431-9dddcece7f88?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+const projectImage2 =
+  "https://images.unsplash.com/photo-1575540325855-4b5d285a3845?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGFuaW1lfGVufDB8fDB8fHwy";
+const arrayOfImages = [projectImage, projectImage2];
+const projectsData = [
+  {
+    name: "Project 1",
+    description: "This is a project description",
+    state: "In progress",
+    githublink: " ",
+    weblink: " ",
+    technologies: ["React", "Node", "Express", "MongoDB"],
+    images: [arrayOfImages],
+  },
+  {
+    name: "Project 2",
+    description: "This is a project description",
+    state: "In progress",
+    githublink: " ",
+    weblink: " ",
+    technologies: ["React", "Node", "Express", "MongoDB"],
+    images: [arrayOfImages],
+  },
+  {
+    name: "Project 3",
+    description: "This is a project description",
+    state: "In progress",
+    githublink: " ",
+    weblink: " ",
+    technologies: ["React", "Node", "Express", "MongoDB"],
+    images: [arrayOfImages],
+  },
+  {
+    name: "Project 4",
+    description: "This is a project description",
+    state: "In progress",
+    githublink: " ",
+    weblink: " ",
+    technologies: ["React", "Node", "Express", "MongoDB"],
+    images: [arrayOfImages],
+  },
+  {
+    name: "Project 5",
+    description: "This is a project description",
+    state: "In progress",
+    githublink: " ",
+    weblink: " ",
+    technologies: ["React", "Node", "Express", "MongoDB"],
+    images: [arrayOfImages],
+  },
+  {
+    name: "Project 6",
+    description: "This is a project description",
+    state: "In progress",
+    githublink: " ",
+    weblink: " ",
+    technologies: ["React", "Node", "Express", "MongoDB"],
+    images: [arrayOfImages],
+  },
 ];
 
 export const sideBarSections = [
@@ -86,7 +166,7 @@ export const sideBarSections = [
   {
     title: "Projects",
     id: "Projects",
-    content: text,
+    content: <Projects projectsData={projectsData} />,
     icon: "workIcon",
     label: "Projects",
   },
@@ -131,5 +211,3 @@ export const socialLinks = [
     typeIcon: null,
   },
 ];
-
-
