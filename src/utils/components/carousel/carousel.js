b8a6d84508bf6b2a895eb2b89a8a778fde336f95
Joram Mumb <jorammumb15.jm@gmail.com>
2024-02-15 12:13:50 +0100

    update carousel

--- src/utils/components/carousel/carousel.js
+++ src/utils/components/carousel/carousel.js
@@ -12,19 +12,19 @@ export const MyCarousel = () => {
       title: "Baseball",
       description:
         "Baseball is a bat-and-ball sport played between two teams of nine players each, taking turns batting and fielding. The game occurs over the course of several plays, with each play generally beginning when a player on the fielding team, called the pitcher.",
-      image: require("../../../assets/images/PictureOfMe.jpg"),
+      image: require("../../../assets/images/example1.svg"),
     },
     {
       title: "Walking",
       description:
         "Walking (also known as ambulation) is one of the main gaits of terrestrial locomotion among legged animals. Walking is typically slower than running and other gaits. ",
-      image: require("../../../assets/images/PictureOfMe.jpg"),
+      image: require("../../../assets/images/example2.svg"),
     },
     {
       title: "Weights",
       description:
         "Weightlifting generally refers to activities in which people lift weights, often in the form of dumbbells or barbells. People lift various kinds of weights for a variety of different reasons.",
-      image: require("../../../assets/images/PictureOfMe.jpg"),
+      image: require("../../../assets/images/example3.svg"),
     },
   ];
 
@@ -37,51 +37,42 @@ export const MyCarousel = () => {
 
     setActiveIndex(newIndex);
   };
- 
+
   return (
     <div className="myCarousel">
       <div
         className="inner"
-        style={{ transform: `translate(-${activeIndex * 100}%)` }}
+        style={{ transform: `translateX(-${activeIndex * 100}%)` }}
       >
-        {items.map((item, index) => {
-          return <CarouselItem key={index} item={item} width={"100%"} />;
-        })}
+        {items.map((item, index) => (
+          <CarouselItem key={index} item={item} width={"100%"} index={index} />
+        ))}
       </div>
-
       <div className="myCarousel-buttons">
         <button
           className="button-arrow"
-          onClick={() => {
-            updateIndex(activeIndex - 1);
-          }}
+          onClick={() => updateIndex(activeIndex - 1)}
         >
           <ArrowBack />
         </button>
         <div className="indicators">
-          {items.map((item, idx) => {
-            return (
-              <button
-                key={idx}
-                className="indicator-buttons"
-                onClick={() => {
-                  updateIndex(idx);
-                }}
-              >
-                {idx === activeIndex ? (
-                  <RadioButtonChecked />
-                ) : (
-                  <RadioButtonUnchecked />
-                )}
-              </button>
-            );
-          })}
+          {items.map((item, idx) => (
+            <button
+              key={idx}
+              className="indicator-buttons"
+              onClick={() => updateIndex(idx)}
+            >
+              {idx === activeIndex ? (
+                <RadioButtonChecked />
+              ) : (
+                <RadioButtonUnchecked />
+              )}
+            </button>
+          ))}
         </div>
         <button
           className="button-arrow"
-          onClick={() => {
-            updateIndex(activeIndex + 1);
-          }}
+          onClick={() => updateIndex(activeIndex + 1)}
         >
           <ArrowForward />
         </button>
--- src/utils/components/carousel/carouselItems.js
+++ src/utils/components/carousel/carouselItems.js
@@ -1,13 +1,20 @@
 import React from "react";
 import "../carousel/myCarousel.css";
-export const CarouselItem = ({ item, width }) => {
+export const CarouselItem = ({ item, index }) => {
+  const isEvenIndex = index % 2 === 0;
 
   return (
-    <div  className="myCarousel-item" style={{ width: width }}>
+    <div
+      className="myCarousel-item"
+      style={{
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: isEvenIndex ? "row" : "row-reverse",
+      }}
+    >
       <div></div>
-      <img className="myCarousel-img" src={item.image} alt={'icon'}  />
+      <img className="myCarousel-img" src={item.image.default} alt={"icon"} />
       <div className="myCarousel-item-text">{item.description}</div>
-    
     </div>
   );
 };
